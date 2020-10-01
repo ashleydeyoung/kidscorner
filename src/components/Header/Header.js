c7e53f13ccec2b0c9082de1e7cc3acb3cf7dd489
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-10-01 13:13:57 -0400

    Header and wrapper

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Header(props) {
+  return (
+    <div className="jumbotron">
+      <h1 >Time Out Spinner</h1>
+      <p>
+        Oh No! You're in Time-OUT!
+      </p>
+      <p>
+        Spin to see how many seconds you'll stay!
+      </p>
+    </div>
+  );
+}
+
+export default Header;
