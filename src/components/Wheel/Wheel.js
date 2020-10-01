f6ac809dff628c9633bdb04b929bc5866ee90363
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-10-01 15:28:51 -0400

    slight changes to structure of wheel container

--- src/components/Wheel/Wheel.js
+++ src/components/Wheel/Wheel.js
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "../../images/down.png"
+import img from "../../images/down.png";
 
 import "./Wheel.css";
 
@@ -20,41 +20,43 @@ class Wheel extends React.Component {
   };
   render() {
     return (
-      <div id="mainbox" className="mainbox">
-        <div className="button mt-4">
-          <button className="btn btn-danger spin" onClick={this.handleSpin}>
-            SPIN
-          </button>
-        </div>
-          <div>
-          <img className="arrow" src= {img} alt="arrow"/>
+      <div className="contaniner">
+          <div className="button mt-4">
+            <button className="btn btn-danger spin" onClick={this.handleSpin}>
+              SPIN
+            </button>
           </div>
-        <div id="box" className="box">
-          <div className="box1">
-            <span className="span1">
-              <b>30</b>
-            </span>
-            <span className="span2">
-              <b>45</b>
-            </span>
-            <span className="span3">
-              <b>60</b>
-            </span>
-            <span className="span4">
-              <b>75</b>
-            </span>
-            <span className="span5">
-              <b>20</b>
-            </span>
-            <span className="span6">
-              <b>0</b>
-            </span>
-            <span className="span7">
-              <b>90</b>
-            </span>
-            <span className="span8">
-              <b>100</b>
-            </span>
+          <div>
+            <img className="arrow" src={img} alt="arrow" />
+          </div>
+        <div id="mainbox" className="mainbox">
+          <div id="box" className="box container">
+            <div className="box1">
+              <span className="span1">
+                <b>30</b>
+              </span>
+              <span className="span2">
+                <b>45</b>
+              </span>
+              <span className="span3">
+                <b>60</b>
+              </span>
+              <span className="span4">
+                <b>75</b>
+              </span>
+              <span className="span5">
+                <b>20</b>
+              </span>
+              <span className="span6">
+                <b>0</b>
+              </span>
+              <span className="span7">
+                <b>90</b>
+              </span>
+              <span className="span8">
+                <b>100</b>
+              </span>
+            </div>
           </div>
         </div>
       </div>
