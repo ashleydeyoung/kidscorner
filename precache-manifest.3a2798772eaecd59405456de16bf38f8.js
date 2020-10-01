b4ba416e51e008fe41172960337634a765e7be89
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-10-01 13:20:28 -0400

    Updates

---
+++ precache-manifest.3a2798772eaecd59405456de16bf38f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1223b5bd16a08acfb766f5a1e17ba6ed",
+    "url": "/kidscorner/index.html"
+  },
+  {
+    "revision": "9f4676aef24086b9c01f",
+    "url": "/kidscorner/static/css/main.91a9249c.chunk.css"
+  },
+  {
+    "revision": "2ab17587a561f96e1790",
+    "url": "/kidscorner/static/js/2.df36f318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kidscorner/static/js/2.df36f318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4676aef24086b9c01f",
+    "url": "/kidscorner/static/js/main.67d6d992.chunk.js"
+  },
+  {
+    "revision": "4382f42ee7fcf5df088f",
+    "url": "/kidscorner/static/js/runtime-main.758ca9ba.js"
+  },
+  {
+    "revision": "2f6a8f296d7fcc320f8310773ae24af9",
+    "url": "/kidscorner/static/media/down.2f6a8f29.png"
+  }
+]);
